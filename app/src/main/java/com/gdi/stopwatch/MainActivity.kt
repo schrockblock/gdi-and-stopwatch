f538c145c0461bdfbb4e67ae39b3af7762c01f75
Elliot <elliot.schrock@gmail.com>
2017-09-07 17:37:52 -0400

    connect layout to code

--- app/src/main/java/com/gdi/stopwatch/MainActivity.kt
+++ app/src/main/java/com/gdi/stopwatch/MainActivity.kt
@@ -2,11 +2,20 @@ package com.gdi.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var timeTextView: TextView? = null
+    var startButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        timeTextView = findViewById(R.id.tv_time)
+        startButton = findViewById(R.id.btn_start)
+
+        startButton?.setOnClickListener { startButton?.setText("Stop") }
     }
 }
