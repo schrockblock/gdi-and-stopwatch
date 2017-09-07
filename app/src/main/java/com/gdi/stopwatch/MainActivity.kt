6c4cc807ba207362abab71767924053e9f7de8ca
Elliot <elliot.schrock@gmail.com>
2017-09-07 18:57:19 -0400

    make timer work

--- app/src/main/java/com/gdi/stopwatch/MainActivity.kt
+++ app/src/main/java/com/gdi/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gdi.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
 
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     var startButton: Button? = null
 
     var isTimerRunning: Boolean = false
+    var startTime: Long? = null
+    var timerHandler: Handler = Handler()
+    var timerAction: (() -> Unit) = {
+        if (isTimerRunning) updateTimerText()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,25 @@ class MainActivity : AppCompatActivity() {
 
         startButton?.setOnClickListener {
             isTimerRunning = !isTimerRunning
+            if (isTimerRunning) {
+                startTime = System.currentTimeMillis()
+                timerHandler.postDelayed(timerAction, 0)
+            } else {
+                timerHandler.removeCallbacks(timerAction)
+            }
             startButton?.setText(if (isTimerRunning) "Stop" else "Start")
         }
     }
+
+    fun updateTimerText() {
+        val currentTime = System.currentTimeMillis()
+        val timeSince = currentTime - (startTime ?: currentTime)
+        timeTextView?.setText(timeSince.timeIntervalString())
+        timerHandler.postDelayed(timerAction, 30)
+    }
 }
+
+fun Long.toCentiseconds() = this / 10
+fun Long.toSeconds() = this / 1000
+fun Long.toMinutes() = this.toSeconds() / 60
+fun Long.timeIntervalString() = String.format("%02d:%02d:%02d", this.toMinutes(), this.toSeconds() % 60, this.toCentiseconds() % 100)
